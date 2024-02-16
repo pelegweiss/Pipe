3a95987495202fc9e4529f5f5580591e4968d44d
Pelegweiss <pelegw200@gmail.com>
2024-02-16 17:26:09 +0200

    removed overload member as it's not needed

--- Pipe.h
+++ Pipe.h
@@ -46,7 +46,4 @@ public:
     pipeMessage readPipeMessage();
     bool sendPacketMessage(const pipeMessage& message);
     bool sendBlockHeaderMessage(const pipeMessage& message);
-private:
-    OVERLAPPED overlapped; // Declare overlapped as a member variable
-
 };
